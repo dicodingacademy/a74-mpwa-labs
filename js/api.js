cc5e8b34e1fff506d28027fbed418581332d347f
Bagus Aji Santoso <lobothijau@gmail.com>
2018-11-30 10:10:39 +0700

    Moving news reader example app to repo

---
+++ js/api.js
@@ -0,0 +1,132 @@
+var base_url = "https://readerapi.codepolitan.com/";
+
+// Blok kode yang akan di panggil jika fetch berhasil
+function status(response) {
+  if (response.status !== 200) {
+    console.log("Error : " + response.status);
+    // Method reject() akan membuat blok catch terpanggil
+    return Promise.reject(new Error(response.statusText));
+  } else {
+    // Mengubah suatu objek menjadi Promise agar bisa "di-then-kan"
+    return Promise.resolve(response);
+  }
+}
+
+// Blok kode untuk memparsing json menjadi array JavaScript
+function json(response) {
+  return response.json();
+}
+
+// Blok kode untuk meng-handle kesalahan di blok catch
+function error(error) {
+  // Parameter error berasal dari Promise.reject()
+  console.log("Error : " + error);
+}
+
+// Blok kode untuk melakukan request data json
+function getArticles() {
+  if ("caches" in window) {
+    caches.match(base_url + "articles").then(function(response) {
+      if (response) {
+        response.json().then(function(data) {
+          var articlesHTML = "";
+          data.result.forEach(function(article) {
+            articlesHTML += `
+                  <div class="card">
+                    <a href="./article.html?id=${article.id}">
+                      <div class="card-image waves-effect waves-block waves-light">
+                        <img src="${article.thumbnail}" />
+                      </div>
+                    </a>
+                    <div class="card-content">
+                      <span class="card-title truncate">${article.title}</span>
+                      <p>${article.description}</p>
+                    </div>
+                  </div>
+                `;
+          });
+          // Sisipkan komponen card ke dalam elemen dengan id #content
+          document.getElementById("articles").innerHTML = articlesHTML;
+        });
+      }
+    });
+  }
+
+  fetch(base_url + "articles")
+    .then(status)
+    .then(json)
+    .then(function(data) {
+      // Objek/array JavaScript dari response.json() masuk lewat data.
+
+      // Menyusun komponen card artikel secara dinamis
+      var articlesHTML = "";
+      data.result.forEach(function(article) {
+        articlesHTML += `
+              <div class="card">
+                <a href="./article.html?id=${article.id}">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img src="${article.thumbnail}" />
+                  </div>
+                </a>
+                <div class="card-content">
+                  <span class="card-title truncate">${article.title}</span>
+                  <p>${article.description}</p>
+                </div>
+              </div>
+            `;
+      });
+      // Sisipkan komponen card ke dalam elemen dengan id #content
+      document.getElementById("articles").innerHTML = articlesHTML;
+    })
+    .catch(error);
+}
+
+function getArticleById() {
+  // Ambil nilai query parameter (?id=)
+  var urlParams = new URLSearchParams(window.location.search);
+  var idParam = urlParams.get("id");
+
+  if ("caches" in window) {
+    caches.match(base_url + "article/" + idParam).then(function(response) {
+      if (response) {
+        response.json().then(function(data) {
+          var articleHTML = `
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img src="${data.result.cover}" />
+              </div>
+              <div class="card-content">
+                <span class="card-title">${data.result.post_title}</span>
+                ${snarkdown(data.result.post_content)}
+              </div>
+            </div>
+          `;
+          // Sisipkan komponen card ke dalam elemen dengan id #content
+          document.getElementById("body-content").innerHTML = articleHTML;
+        });
+      }
+    });
+  }
+
+  fetch(base_url + "article/" + idParam)
+    .then(status)
+    .then(json)
+    .then(function(data) {
+      // Objek JavaScript dari response.json() masuk lewat variabel data.
+      console.log(data);
+      // Menyusun komponen card artikel secara dinamis
+      var articleHTML = `
+          <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img src="${data.result.cover}" />
+            </div>
+            <div class="card-content">
+              <span class="card-title">${data.result.post_title}</span>
+              ${snarkdown(data.result.post_content)}
+            </div>
+          </div>
+        `;
+      // Sisipkan komponen card ke dalam elemen dengan id #content
+      document.getElementById("body-content").innerHTML = articleHTML;
+    });
+}
--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,60 @@
-const CACHE_NAME = 'firstpwa-v4';
+const CACHE_NAME = "firstpwa-v1";
 var urlsToCache = [
-	'/',
-	'/nav.html',
-	'/index.html',
-	'/pages/home.html',
-	'/pages/about.html',
-	'/pages/contact.html',
-	'/css/materialize.min.css',
-	'/js/materialize.min.js',
-	'/js/script.js'
+  "/",
+  "/nav.html",
+  "/index.html",
+  "/article.html",
+  "/pages/home.html",
+  "/pages/about.html",
+  "/pages/contact.html",
+  "/css/materialize.min.css",
+  "/js/materialize.min.js",
+  "/manifest.json",
+  "/js/nav.js",
+  "/js/api.js",
+  "/icon.png"
 ];
 
-self.addEventListener('install', function(event){
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-		.then(function(cache) {
-			return cache.addAll(urlsToCache);
-		})
-	);
-})
-
-self.addEventListener('activate', function(event){
-	event.waitUntil(
-		caches.keys()
-		.then(function(cacheNames) {
-			return Promise.all(
-				cacheNames.map(function(cacheName){
-					if(cacheName != CACHE_NAME){	
-						console.log("ServiceWorker: cache " + cacheName + " dihapus");
-						return caches.delete(cacheName);
-					}
-				})
-			);
-		})
-	);
-})
-
-self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches.match(event.request, {cacheName:CACHE_NAME})
-		.then(function(response) {
-			if(response){
-				console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
-				return response;
-			}
-			
-			console.log("ServiceWorker: Memuat aset dari server: ", event.request.url);
-			return fetch(event.request);
-		})
-	);
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
+self.addEventListener("fetch", function(event) {
+  var base_url = "https://readerapi.codepolitan.com/";
+
+  if (event.request.url.indexOf(base_url) > -1) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return fetch(event.request).then(function(response) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        })
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request, { ignoreSearch: true }).then(function(response) {
+        return response || fetch (event.request);
+      })
+    )
+  }
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
